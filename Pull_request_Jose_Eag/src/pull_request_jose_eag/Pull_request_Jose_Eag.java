092de152831c3c34dd025e49134d01625c5460c7
andresmartinez6 <andresiz11x@gmail.com>
2023-05-11 16:39:02 +0200

    Sout andres

---
+++ Pull_request_Jose_Eag/src/pull_request_jose_eag/Pull_request_Jose_Eag.java
@@ -0,0 +1,15 @@
+
+package pull_request_jose_eag;
+import java.util.Scanner;
+
+public class Pull_request_Jose_Eag {
+
+    
+    public static void main(String[] args) {
+        Scanner teclado=new Scanner(System.in);
+        
+        
+        System.out.println("Hola soy Andrés");
+    }
+    
+}
